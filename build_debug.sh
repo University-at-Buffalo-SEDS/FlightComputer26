5c11839f4bcf7179832d75e1038823942190b861
Fuad Veliev <174904186+flatmoll@users.noreply.github.com>
2025-11-05 11:53:00 -0500

    Remove build script as requested.

--- build_debug.sh
+++
@@ -1,3 +0,0 @@
-cmake -DCMAKE_BUILD_TYPE="debug" -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -S $(pwd) -B "build/Debug" -G Ninja
-cmake --build build/Debug --parallel
-arm-none-eabi-objcopy -O binary "build/Debug/FlightComputer26.elf" "build/Debug/FlightComputer26.bin" 
