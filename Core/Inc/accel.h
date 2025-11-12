1a7b93eda182ec3bc8209d02feb5c89941a2e0dd
Fuad Veliev <174904186+flatmoll@users.noreply.github.com>
2025-11-11 19:44:10 -0500

    I am becoming fancy!

--- Core/Inc/accel.h
+++ Core/Inc/accel.h
@@ -20,6 +20,7 @@
 #define ACCEL_RESET_VAL     0xB6
 #define ACCEL_RANGE_VAL     0x03
 #define ACCEL_CONF_VAL      0x28
+#define ACCEL_BUF_SIZE      6
 
 #define ACCEL_CMD_READ(reg)  ((uint8_t)((reg) | 0x80u))
 #define ACCEL_CMD_WRITE(reg) ((uint8_t)((reg) & ~0x80u))
