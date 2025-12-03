3bc77e78262e10fcfa4af6a15d5bcb865dfee790
Fuad Veliev <174904186+flatmoll@users.noreply.github.com>
2025-12-03 16:41:58 -0500

    dma: style update (header)

--- Core/Inc/dma-ring.h
+++ Core/Inc/dma-ring.h
@@ -1,6 +1,4 @@
-/**
- * Compile-time definitions related to DMA and ring buffer.
- */
+// Includes consumer API
 
 #include "stm32h5xx_hal.h"
 #include "stm32h5xx_hal_gpio.h"
@@ -9,18 +7,20 @@
 #include "gyro.h"
 #include "accel.h"
 
-// DMA interrupt pins for each device
+/* DMA interrupt pins for each device */
+
 #define ACCEL_INT_PIN_1 GPIO_PIN_4
 #define ACCEL_INT_PIN_2 GPIO_PIN_5
 #define GYRO_INT_PIN_1  GPIO_PIN_0
 #define GYRO_INT_PIN_2  GPIO_PIN_1
 #define BARO_INT_PIN    GPIO_PIN_7
 
-// Ring properties
+/* Ring properties */
+
 #define RING_SIZE 16
 #define RING_MASK RING_SIZE - 1
 
-// Packet queue typedefs
+/* Packet queue typedefs */
 
 typedef enum {
   NONE,
@@ -38,18 +38,14 @@ typedef struct {
   } data;
 } payload_t;
 
-// Global functions
+/* Globals */
 
-/**
- * @brief Dequeue the oldest element of the ring and advance tail.
- * @param None
- * @retval A boolean indicating success or that the ring is empty.
+/*
+ * Dequeue the oldest element of the ring and advance tail.
  */
 inline bool dma_ring_dequeue_oldest(payload_t *buf);
 
-/**
- * @brief Copy but not dequeue the oldest element of the ring.
- * @param None
- * @retval A boolean indicating success or that the ring is empty.
+/*
+ * Copy but not dequeue the oldest element of the ring.
  */
 inline bool dma_ring_copy_oldest(payload_t *buf);
