97cf41bc150d85f799f0166393fad3c031210b83
Fuad Veliev <174904186+flatmoll@users.noreply.github.com>
2025-12-18 22:22:57 -0500

    dma: double buffers skeleton (no sync)

--- Core/Inc/dma.h
+++ Core/Inc/dma.h
@@ -1,45 +1,45 @@
 // Includes consumer API
 
 #include <stdint.h>
+#include <stdatomic.h>
 
-/* Ring properties */
+#define SENSOR_BUF_SIZE 8
 
-#define RING_SIZE 16
-#define RING_MASK RING_SIZE - 1
-
-/* Status reporting to consumers */
+/* Typedefs */
 
 typedef enum {
-  DMA_OK      = 0,
-  RING_EMPTY  = -1,
-} dma_e;
-
-/* Packet queue typedefs */
-
-typedef enum {
-  NONE,
-  BAROMETER,
-  GYROSCOPE,
-  ACCELEROMETER,
+  NONE = 0,
+  BAROMETER = 1,
+  GYROSCOPE = 2,
+  ACCELEROMETER = 3,
 } expected_e;
 
 typedef struct {
   expected_e type;
   union {
-    float baro[3];
+    float baro[2];
     int16_t gyro[3];
     float accel[3];
   } data;
 } payload_t;
 
+typedef enum {
+  DMA_OK,
+  DMA_BUSY,
+  DMA_EMPTY,
+  DMA_GENERR
+} dma_e;
+
+typedef struct {
+  atomic_uint_fast8_t w;
+  atomic_uint_fast8_t r;
+  expected_e type[2];
+} dma_t;
+
 /* Globals */
 
 /*
- * Dequeue the oldest element of the ring and advance tail.
+ * Reads the latest payload entry.
+ * Returns DMA_OK (0) on success and > 0 on failure.
  */
-inline dma_e dma_ring_dequeue_oldest(payload_t *buf);
-
-/*
- * Copy but not dequeue the oldest element of the ring.
- */
-inline dma_e dma_ring_copy_oldest(payload_t *buf);
+inline dma_e dma_read_latest(payload_t *buf);
--- Core/Src/dma.c
+++ Core/Src/dma.c
@@ -9,231 +9,110 @@
 #include "platform.h"
 #include "dma.h"
 
-/* 
- * DMA buffers for each sensor.
- * Note: HAL expects non-volatile buffers.
- * Tx: Producer: (final value). Consumer: DMA peripheral.
- * Rx: Producer: DMA peripheral. Consumer: ReceiveComplete callback.
- */
-static uint8_t baro_dma_tx[BMP390_BUF_SIZE] = {[0] = BARO_TX_BYTE};
-static uint8_t baro_dma_rx[BMP390_BUF_SIZE];
-static uint8_t gyro_dma_tx[GYRO_BUF_SIZE] = {[0] = GYRO_TX_BYTE};
-static uint8_t gyro_dma_rx[GYRO_BUF_SIZE];
-static uint8_t accel_dma_tx[ACCEL_BUF_SIZE] = {[0] = ACCEL_TX_BYTE};
-static uint8_t accel_dma_rx[ACCEL_BUF_SIZE];
-
-/* 
- * Ring buffer. Producer: ReceiveComplete callback.
- * Consumers: Sensor and Kalman tasks.
- */
-static volatile payload_t ring[RING_SIZE];
-static volatile atomic_uint_fast16_t head = ATOMIC_VAR_INIT(0);
-static volatile atomic_uint_fast16_t tail = ATOMIC_VAR_INIT(0);
-
 /*
- * Device to expect next time ReceiveComplete is invoked by HAL.
+ * Control struct and tx/rx buffers.
+ * Two shared rx bufs implement double buffering.
  */
-static volatile atomic_uint_fast8_t next = ATOMIC_VAR_INIT(NONE);
-static uint_fast8_t expected = ATOMIC_VAR_INIT(NONE);
+static dma_t ctr = {0};
 
-/*
- * Enqueues data and signals transfer completion.
- */
-static inline void enqueue(const expected_e type)
+static uint8_t tx[3][SENSOR_BUF_SIZE] = {
+  [0][0] = BARO_TX_BYTE, [1][0] = GYRO_TX_BYTE, [2][0] = ACCEL_TX_BYTE
+};
+static uint8_t rx[2][SENSOR_BUF_SIZE];
+
+static inline uint_fast8_t index_from_ptr(uint8_t *p)
 {
-  uint_fast16_t h = atomic_load_explicit(&head, memory_order_acquire);
-  uint_fast16_t t = atomic_load_explicit(&tail, memory_order_acquire);
-  uint_fast16_t i = h & RING_MASK;
+  return (p != NULL) ? ((p == rx[0]) ? 0 : 1) : 127;
+}
 
-  /* 
-   * If the ring is full, overwrite oldest entry
-   * to keep slow consumer updated.
-   */
-  if (((h + 1) & RING_MASK) == (t & RING_MASK))
-    atomic_store_explicit(&tail, t + 1, memory_order_release);
-
-  switch (type) {
+static inline void switch_pull_cs_high(uint_fast8_t i)
+{
+  switch (ctr.type[i]) {
     case BAROMETER:
-    {
-      INVALIDATE_DCACHE_ADDR_INT(baro_dma_rx, PL_BUF_SIZE_BARO);
-
-      ring[i].type = BAROMETER;
-      ring[i].data.baro[0] = U24(baro_dma_rx[1], baro_dma_rx[2], baro_dma_rx[3]);
-      ring[i].data.baro[1] = U24(baro_dma_rx[4], baro_dma_rx[5], baro_dma_rx[6]);
-
-      PL_CS_BARO_HIGH();
-      break;
-    }
+      PL_CS_BARO_HIGH(); break;
     case GYROSCOPE:
-    {
-      INVALIDATE_DCACHE_ADDR_INT(gyro_dma_rx, PL_BUF_SIZE_GYRO); 
-
-      ring[i].type = GYROSCOPE;
-      ring[i].data.gyro[0] = I16(gyro_dma_rx[1], gyro_dma_rx[2]);
-      ring[i].data.gyro[1] = I16(gyro_dma_rx[3], gyro_dma_rx[4]);
-      ring[i].data.gyro[2] = I16(gyro_dma_rx[5], gyro_dma_rx[6]);
-
-      PL_CS_GYRO_HIGH();
-      break;
-    }
+      PL_CS_GYRO_HIGH(); break;
     case ACCELEROMETER:
-    {
-      INVALIDATE_DCACHE_ADDR_INT(accel_dma_rx, PL_BUF_SIZE_ACCEL);
-
-      ring[i].type = ACCELEROMETER;
-      ring[i].data.accel[0] = F16(accel_dma_rx[2], accel_dma_rx[3]);
-      ring[i].data.accel[1] = F16(accel_dma_rx[4], accel_dma_rx[5]);
-      ring[i].data.accel[2] = F16(accel_dma_rx[6], accel_dma_rx[7]);
-
-      PL_CS_ACCEL_HIGH();
-      break;
-    }
+      PL_CS_ACCEL_HIGH(); break;
     case NONE: return;
   }
-
-  atomic_store_explicit(&head, h + 1, memory_order_release);
 }
 
 /*
- * Copy the oldest raw element of the ring.
+ * TODO
  */
-inline dma_e dma_ring_copy_oldest(payload_t *buf)
+void HAL_SPI_TxRxCpltCallback(PL_SPI_Handle *hspi)
 {
-  uint16_t h = atomic_load_explicit(&head, memory_order_acquire);
-  uint16_t t = atomic_load_explicit(&tail, memory_order_relaxed);
-  uint16_t i = t & RING_MASK;
+  uint_fast8_t a = index_from_ptr(hspi->pRxBuffPtr);
+  if (a == 127) return;
 
-  if ((h & RING_MASK) == i)
-    return RING_EMPTY;
-
-  DISABLE_HAL_INTS();
-  *buf = ring[i];
-  ENABLE_HAL_INTS();
-
-  return DMA_OK;
+  INVALIDATE_DCACHE_ADDR_INT(rx[a], SENSOR_BUF_SIZE);
+  switch_pull_cs_high(a);
 }
 
 /*
- * Dequeue the oldest raw element of the ring and advance tail.
- */
-inline dma_e dma_ring_dequeue_oldest(payload_t *buf)
-{
-  uint16_t h = atomic_load_explicit(&head, memory_order_acquire);
-  uint16_t t = atomic_load_explicit(&tail, memory_order_acquire);
-  uint16_t i = t & RING_MASK;
-
-  /*
-   * Check if the ring is empty (head is on tail)
-   */
-  if ((h & RING_MASK) == i)
-    return RING_EMPTY;
-
-  DISABLE_HAL_INTS();
-  *buf = ring[i];
-  ENABLE_HAL_INTS();
-
-  atomic_store_explicit(&tail, t + 1, memory_order_release);
-  return DMA_OK;
-}
-
-/*
- * Enqueues data and signals transfer completion.
- */
-void HAL_SPI_RxCpltCallback(SPI_HandleTypeDef *hspi)
-{
-  if (hspi->Lock == HAL_LOCKED)
-    return;
-
-  enqueue(atomic_load_explicit(&next, memory_order_acquire));
-  atomic_store_explicit(&next, NONE, memory_order_release);
-}
-
-/*
- * Cleans cache for a device that produced an error.
+ * TODO
  */
 void HAL_SPI_ErrorCallback(PL_SPI_Handle *hspi)
 {
-  if (hspi->Lock == HAL_LOCKED)
-    return;
+  uint_fast8_t a = index_from_ptr(hspi->pRxBuffPtr);
+  if (a == 127) return;
 
-  expected_e t = atomic_load_explicit(&next, memory_order_acquire);
-  atomic_store_explicit(&next, NONE, memory_order_release);
-
-  switch (t) {
-    case BAROMETER:
-    {
-      CLEAN_DCACHE_ADDR_INT(baro_dma_rx, PL_BUF_SIZE_BARO);
-      PL_CS_BARO_HIGH();
-      break;
-    }
-    case GYROSCOPE:
-    {
-      CLEAN_DCACHE_ADDR_INT(gyro_dma_rx, PL_BUF_SIZE_GYRO);
-      PL_CS_GYRO_HIGH();
-      break;
-    }
-    case ACCELEROMETER:
-    {
-      CLEAN_DCACHE_ADDR_INT(accel_dma_rx, PL_BUF_SIZE_ACCEL);
-      PL_CS_ACCEL_HIGH();
-      break;
-    }
-    case NONE: return;
-  }
+  CLEAN_DCACHE_ADDR_INT(rx[a], SENSOR_BUF_SIZE);
+  switch_pull_cs_high(a);
 }
 
 /*
- * Interrupt used to trigger DMA transfer
+ * TODO
  */
 void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin)
 {
-  PL_HAL_Handle st;
+  uint_fast8_t a = atomic_fetch_xor_explicit(&ctr.w, 1u, memory_order_acq_rel) & 1u;
 
   switch (GPIO_Pin) {
     case BARO_INT_PIN:
-    {
-      if (!atomic_compare_exchange_strong(&next, &expected, BAROMETER))
-        return;
-
+      ctr.type[a] = BAROMETER;
       PL_CS_BARO_LOW();
-      st = DMA_SPI_TXRX(baro_dma_tx, baro_dma_rx, PL_BUF_SIZE_BARO);
-
-      if (st != HAL_OK) {
-        PL_CS_BARO_HIGH();
-        atomic_store_explicit(&next, NONE, memory_order_release);
-      }
+      DMA_SPI_TXRX(tx[0], rx[a], SENSOR_BUF_SIZE);
       break;
-    }
     case GYRO_INT_PIN_1:
     case GYRO_INT_PIN_2:
-    {
-      if (!atomic_compare_exchange_strong(&next, &expected, GYROSCOPE))
-        return;
-
+      ctr.type[a] = GYROSCOPE;
       PL_CS_GYRO_LOW();
-      st = DMA_SPI_TXRX(gyro_dma_tx, gyro_dma_rx, PL_BUF_SIZE_GYRO);
-
-      if (st != HAL_OK) {
-        PL_CS_GYRO_HIGH();
-        atomic_store_explicit(&next, NONE, memory_order_release);
-      }
+      DMA_SPI_TXRX(tx[1], rx[a], SENSOR_BUF_SIZE);
       break;
-    }
     case ACCEL_INT_PIN_1:
     case ACCEL_INT_PIN_2:
-    {
-      if (!atomic_compare_exchange_strong(&next, &expected, ACCELEROMETER))
-        return;
-
+      ctr.type[a] = ACCELEROMETER;
       PL_CS_ACCEL_LOW();
-      st = DMA_SPI_TXRX(accel_dma_tx, accel_dma_rx, PL_BUF_SIZE_ACCEL);
-
-      if (st != HAL_OK) {
-        PL_CS_ACCEL_HIGH();
-        atomic_store_explicit(&next, NONE, memory_order_release);
-      }
+      DMA_SPI_TXRX(tx[2], rx[a], SENSOR_BUF_SIZE);
       break;
-    }
     default: return;
   }
+}
+
+inline dma_e dma_read_latest(payload_t *buf)
+{
+  uint_fast8_t a = atomic_load_explicit(&ctr.w, memory_order_acquire);
+  buf->type = ctr.type[a];
+  
+  switch (buf->type) {
+    case BAROMETER:
+      buf->data.baro[0] = U24(rx[a][1], rx[a][2], rx[a][3]);
+      buf->data.baro[1] = U24(rx[a][4], rx[a][5], rx[a][6]);
+      break;
+    case GYROSCOPE:
+      buf->data.gyro[0] = I16(rx[a][1], rx[a][2]);
+      buf->data.gyro[1] = I16(rx[a][3], rx[a][4]);
+      buf->data.gyro[2] = I16(rx[a][5], rx[a][6]);
+      break;
+    case ACCELEROMETER:
+      buf->data.accel[0] = F16(rx[a][2], rx[a][3]);
+      buf->data.accel[1] = F16(rx[a][4], rx[a][5]);
+      buf->data.accel[2] = F16(rx[a][6], rx[a][7]);
+      break;
+    case NONE: return DMA_EMPTY;
+  }
+
+  return DMA_OK;
 }
