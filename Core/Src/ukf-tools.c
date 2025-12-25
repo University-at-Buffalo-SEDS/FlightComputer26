9f68395c212749f3301db4184c44f0ababa75738
Fuad Veliev <174904186+flatmoll@users.noreply.github.com>
2025-12-24 20:40:33 -0500

    ukf: less fp in predict

--- Core/Src/ukf-tools.c
+++ Core/Src/ukf-tools.c
@@ -53,8 +53,10 @@ inline void ukf_predict(const state_vec_t *restrict vec,
    * DT, but proper timer should be accessible due to the
    * presence of scheduler and ISR. */
 
-  const float fact1 = 0.5f * DT_S;
-  const float fact2 = fact1 * DT_S;
+  const float fact = 0.5f * DT_S;
+  const float dvx = DT_S * vec->a.x;
+  const float dvy = DT_S * vec->a.y;
+  const float dvz = DT_S * vec->a.z;
 
   next->w.x = vec->w.x;
   next->w.y = vec->w.y;
@@ -64,26 +66,26 @@ inline void ukf_predict(const state_vec_t *restrict vec,
   next->a.y = vec->a.y;
   next->a.z = vec->a.z;
 
-  next->v.x = (DT_S * vec->a.x) + vec->v.x;
-  next->v.y = (DT_S * vec->a.y) + vec->v.y;
-  next->v.z = (DT_S * vec->a.z) + vec->v.z;
+  next->v.x = dvx + vec->v.x;
+  next->v.y = dvy + vec->v.y;
+  next->v.z = dvz + vec->v.z;
   
-  next->p.x = (fact2 * vec->a.x) + (DT_S * vec->v.x) + vec->p.x;
-  next->p.y = (fact2 * vec->a.y) + (DT_S * vec->v.y) + vec->p.y;
-  next->p.z = (fact2 * vec->a.z) + (DT_S * vec->v.z) + vec->p.z;
+  next->p.x = (fact * dvx) + (DT_S * vec->v.x) + vec->p.x;
+  next->p.y = (fact * dvy) + (DT_S * vec->v.y) + vec->p.y;
+  next->p.z = (fact * dvz) + (DT_S * vec->v.z) + vec->p.z;
 
-  next->q1 = vec->q1 - fact1 * (vec->w.x * vec->q2 + \
-                                vec->w.y * vec->q3 + \
-                                vec->w.z * vec->q4);
-  next->q2 = vec->q2 + fact1 * (vec->w.x * vec->q1 + \
-                                vec->w.z * vec->q3 - \
-                                vec->w.y * vec->q4);
-  next->q3 = vec->q3 + fact1 * (vec->w.y * vec->q1 - \
-                                vec->w.z * vec->q2 + \
-                                vec->w.x * vec->q4);
-  next->q4 = vec->q4 + fact1 * (vec->w.z * vec->q1 + \
-                                vec->w.y * vec->q2 - \
-                                vec->w.x * vec->q3);
+  next->q1 = vec->q1 - fact * (vec->w.x * vec->q2 + \
+                               vec->w.y * vec->q3 + \
+                               vec->w.z * vec->q4);
+  next->q2 = vec->q2 + fact * (vec->w.x * vec->q1 + \
+                               vec->w.z * vec->q3 - \
+                               vec->w.y * vec->q4);
+  next->q3 = vec->q3 + fact * (vec->w.y * vec->q1 - \
+                               vec->w.z * vec->q2 + \
+                               vec->w.x * vec->q4);
+  next->q4 = vec->q4 + fact * (vec->w.z * vec->q1 + \
+                               vec->w.y * vec->q2 - \
+                               vec->w.x * vec->q3);
 
   const float expr = next->q1*next->q1 + next->q2*next->q2 + \
                      next->q3*next->q3 + next->q4*next->q4;
