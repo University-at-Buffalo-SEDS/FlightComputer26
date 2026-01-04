f08d3ccdd9a742a88a114ba8dcc19523d35ee143
Fuad Veliev <174904186+flatmoll@users.noreply.github.com>
2026-01-04 14:14:45 -0500

    sll: experimental lock-free spsc exchange medium

---
+++ Core/Src/sll.c
@@ -0,0 +1,86 @@
+// Static Linked List (SLL) for SPSC scenario.
+// Lock-free and conveys order of data to consumer.
+
+#include <stdint.h>
+#include <stdatomic.h>
+
+#include "ukf.h"
+#include "deployment.h"
+
+#define POOL_SIZE 32
+#define POOL_MASK (POOL_SIZE - 1)
+
+typedef struct {
+  filter_t data;
+  uint_fast8_t next;
+} node_t;
+
+node_t pool[POOL_SIZE] = {0};
+
+atomic_uint_fast8_t top = 0u;
+atomic_uint_least32_t mask = 0u;
+
+inline uint_fast8_t sll_select()
+{
+  uint_least32_t m = atomic_load_explicit(&mask, memory_order_acquire);
+
+  for (uint_fast8_t k = POOL_MASK; k >= 0; --k)
+  {
+    uint_least32_t choice = (1u << k);
+
+    if (!(m & choice)) {
+      atomic_fetch_or_explicit(&mask, choice, memory_order_release);
+      return k;
+    }
+  }
+
+  return UINT_FAST8_MAX;
+}
+
+inline void sll_clean(uint_fast8_t curr, uint_fast8_t end)
+{
+  while (curr != end)
+  {
+    uint_fast8_t k = curr;
+    curr = pool[k].next;
+    atomic_fetch_and_explicit(&mask, ~(1u << k), memory_order_release);
+  }
+}
+
+/* Prototypes for producer and consumer */
+
+void sll_produce() // one sample
+{
+  /* Before calling this function, must be certain
+   * that there is data to write to pool. */
+  uint_fast8_t k = sll_select();
+
+  if (k != UINT_FAST8_MAX) {
+    // Write stuff to pool[k].data
+    pool[k].next = atomic_exchange_explicit(&top, k, memory_order_release);
+  }
+}
+
+void sll_consume() // several samples
+{
+  static uint_fast8_t top_cached = 0u;
+
+  uint_fast8_t top_new = atomic_load_explicit(&top, memory_order_acquire);
+  uint_fast8_t t = top_new;
+
+  if (t != top_cached) {
+
+    for (uint_fast8_t i = 0; t != top_cached && i < MAX_SAMPLE; ++i)
+    {
+      uint_fast8_t k = t;
+      t = pool[k].next;
+      // Do stuff with pool[k].data
+      atomic_fetch_and_explicit(&mask, ~(1u << k), memory_order_release);
+    }
+
+    if (t != top_cached)
+      sll_clean(t, top_cached);
+
+    top_cached = top_new;
+  }
+}
