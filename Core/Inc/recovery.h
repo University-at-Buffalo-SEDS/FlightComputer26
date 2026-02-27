a1bd841c4f60939a3e4f6b063d72d6243958c17a
Fuad Veliev <slowguitarist@anche.no>
2026-02-27 14:35:42 -0500

    recovery: adjust timeout values for new tick

--- Core/Inc/recovery.h
+++ Core/Inc/recovery.h
@@ -55,17 +55,17 @@ enum sensor_mask : fu8 {
 
 /* ------ TX Timer interrupt definitions ------ */
 
-/* Time is in ThreadX ticks. 1 tick = 10 ms */
+/* Time is in ThreadX ticks. 1 tick = 1 ms */
 
-#define FC_TIMEOUT 300
-#define GND_TIMEOUT 300
+#define FC_TIMEOUT 3000
+#define GND_TIMEOUT 3000
 
 /* Expiration of timer that invokes timeout checks  */
-#define TX_TIMER_TICKS   50
+#define TX_TIMER_TICKS   500
 #define TX_TIMER_INITIAL (TX_TIMER_TICKS * 2)
 
-#define CO2_ASSERT_INTERVAL  50
-#define REEF_ASSERT_INTERVAL 50
+#define CO2_ASSERT_INTERVAL  500
+#define REEF_ASSERT_INTERVAL 500
 
 /* ------ TX Timer interrupt definitions ------ */
 
