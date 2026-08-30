18e36e414f7c3183fa719332e81aded4172a1062
Rylan Meilutis <rylan.meilutis@gmail.com>
2026-08-30 14:14:05 -0400

    Fix H5 simulator startup and SD layout

--- Core/Src/system_stm32h5xx.c
+++ Core/Src/system_stm32h5xx.c
@@ -144,7 +144,7 @@
      Internal SRAM. */
 /* #define VECT_TAB_SRAM */
 #if !defined(VECT_TAB_OFFSET)
-#define VECT_TAB_OFFSET  0x00U /*!< Vector Table base offset field.
+#define VECT_TAB_OFFSET  0x4200U /*!< Vector Table base offset field.
                                    This value must be a multiple of 0x200. */
 #endif
 /******************************************************************************/
